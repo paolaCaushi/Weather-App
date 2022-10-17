840f7e88e36ed797f012296f27acf7c56cb46de9
paolaCaushi <pcaushi@unitsystem.net>
2022-10-17 10:40:09 +0200

    adding  Weather API

---
+++ scripts/forecast.js
@@ -0,0 +1,17 @@
+//get info from the API 
+
+const key= 'pvfWvyBj26fdYGzTI9vTpTOeucvi8Dxl' ;
+const getCity = async(city) =>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search' ;
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+getCity('Tiranë')
+.then(data => console.log(data))
+.catch(err=> console.log(err));
